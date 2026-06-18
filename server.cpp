13a1f112cdb321ef177b4e1222b364f5cc84c396
BhanuPraharsha <kasaranenibhanupraharsha@gmail.com>
2026-06-18 09:08:35 +0530

    K-V pair logic implemented

--- server.cpp
+++ server.cpp
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #include <vector>
 #include <poll.h>
+#include <map>
 
 #define MAX_MSG_SIZE  32 << 20
 
@@ -17,16 +18,143 @@ using namespace std;
 
 
 /*
-protocol:
-4 bytes message length + 'len' bytes message +...repeat
+message protocol:
+number_of_arguments + 4 bytes message length + 'len' bytes message +...number_of_arguments times
 */
+
+struct response 
+{
+    uint32_t status;
+    vector<uint8_t> data;
+};
+
+/*
+response format :
+status code + data
+*/
+
+
+enum{
+    RES_OK = 0, 
+    RES_ERR = 1, //error
+    RES_NF =  2, //key not found
+};
+
+
+map<string, string> g_map; //global k-v storage
 
 void errorHandler(const char* msg)
 {
-    cout<<"errorHandlerHandler in: ";
+    cout<<"error in: ";
     cout<<msg<<endl;
     abort();
 }
+
+void msg_error(const char* msg)
+{
+    cerr<<msg<<endl;
+    return;
+}
+
+bool read_4bytes(uint8_t **curr, uint8_t *end, uint32_t &out)
+{
+    if(*curr+4 > end) return false;
+
+    memcpy(&out, curr, 4);
+    *curr += 4;
+    return true;
+}
+
+bool read_string(uint8_t **curr, uint8_t *end, size_t n, string out)
+{
+    if(*curr + n > end) return false;
+
+    out.assign(*curr, *curr+n);
+    *curr +=n;
+    return true;
+}
+
+
+void consume_buffer(vector<uint8_t>& buff, size_t n) 
+{
+    buff.erase(buff.begin(), buff.begin()+n);
+    return;
+}
+
+
+
+void append_buffer(vector<uint8_t>& buff, uint8_t* from, size_t n) 
+{
+    buff.insert(buff.end(), from, from+n);
+    return;
+}
+
+
+int parse_request(uint8_t *in, size_t n, vector<string>& out) //parsing the execution commands from the message stream
+{
+    uint32_t nargs;
+    uint8_t *end=in+n;
+
+    if(!read_4bytes(&in, end, nargs)) return -1;
+
+    while(out.size()!=nargs)
+    {
+        uint32_t len;
+        if(!read_4bytes(&in, end, len)) return -1;
+
+        string cmd;
+        if(!read_string(&in, end, len, cmd)) return -1;
+
+        out.push_back(cmd);
+    }
+
+
+    if(in!=end) return -1; //trailing garbage
+
+    return 0;
+}
+
+
+void resolve_request(vector<string>& cmd, response &out) //execution of the commands
+{
+    if(cmd.size()==2 && cmd[0]=="GET")
+    {
+        auto it=g_map.find(cmd[1]);
+        if(it==g_map.end())
+        {
+            out.status==RES_NF;
+            return;
+        }
+
+        out.data.assign(it->second.begin(), it->second.end());
+
+    }
+    else if(cmd.size()==3 && cmd[0]=="SET") 
+    {
+        g_map[cmd[1]]=cmd[2];
+    }
+    else if(cmd.size()==2 & cmd[0]=="DEL")
+    {
+        g_map.erase(cmd[1]);
+    }
+    else{
+        out.status=RES_ERR;
+    }
+    
+    return;
+}
+
+
+void make_response(response &resp, vector<uint8_t> &out) // serialise the response
+{
+    uint32_t len=4 + resp.data.size();
+    append_buffer(out, (uint8_t*)&len, 4);
+    append_buffer(out, (uint8_t*)&resp.status, 4);
+    append_buffer(out, resp.data.data(), resp.data.size());
+
+    return;
+}
+
 
 
 void handleConn(int connfd)
@@ -68,20 +196,6 @@ struct conn{
 };
 
 
-void consume_buffer(vector<uint8_t>& buff, size_t n) 
-{
-    buff.erase(buff.begin(), buff.begin()+n);
-    return;
-}
-
-
-
-void append_buffer(vector<uint8_t>& buff, uint8_t* from, size_t n) 
-{
-    buff.insert(buff.end(), from, from+n);
-    return;
-}
-
 bool try_once(conn* curr) 
 {
     if(curr->incomingBuff.size()<4) return false;
@@ -98,18 +212,23 @@ bool try_once(conn* curr)
     }
 
     if(len+4 > curr->incomingBuff.size()) return false;
-
     uint8_t* request=&curr->incomingBuff[4];
-    cout<<"client says: " <<request<<endl;
+
+    vector<string> cmd;
+    if(parse_request(request, len, cmd)!=0)
+    {
+        curr->want_to_close=true;
+        msg_error("bad request");
+        return false;
+    }
 
 
-    //echo 
-    append_buffer(curr->outgoingBuff, (uint8_t *)&len, 4);
-    append_buffer(curr->outgoingBuff, request, len);
+    response resp;
+    resolve_request(cmd, resp);
+    make_response(resp, curr->outgoingBuff);
+    consume_buffer(curr->outgoingBuff, 4+len);
 
-    consume_buffer(curr->incomingBuff, 4+len);
     return true;
-
 }
 
 
